64863f7899773a190fc714a4886c8e64f18feecc
GabrielScarpelinDiniz <gabriel.diniz@sou.inteli.edu.br>
2025-06-02 15:04:01 -0300

    fix: update some files

--- js/car-viewer.js
+++ js/car-viewer.js
@@ -7,11 +7,9 @@ function setup() {
   const canvas = createCanvas(800, 600, WEBGL);
   canvas.parent("canvas-container");
 
-  // Usar o elemento de cor do HTML
   carColorPicker = select("#nebula-color");
   carColor = color(255, 0, 0);
 
-  // Adicionar evento para o botão de rotação
   setupButton();
 }
 
@@ -19,18 +17,14 @@ function draw() {
   background(30);
   lights();
 
-  // Atualiza a cor com o color picker
   carColor = color(carColorPicker.value());
 
-  // Atualiza o ângulo se a rotação estiver ativada
   if (isRotating) {
     angle += 0.01;
   }
 
-  // Aplica a rotação ao carro
   rotateY(angle);
 
-  // Desenha o carro
   drawCar();
 }
 
@@ -38,17 +32,14 @@ function drawCar() {
   push();
   fill(carColor);
 
-  // Corpo principal do carro
   box(100, 30, 50);
 
-  // Parte superior
   push();
   translate(0, -20, 0);
   fill(200);
   box(60, 20, 45);
   pop();
 
-  // Rodas
   fill(20);
   drawWheel(-40, 20, -25);
   drawWheel(40, 20, -25);
